947111bc9aad9be1f515d11ba66112f83d86899f
Asaf Mor <73069286+asafmor13@users.noreply.github.com>
2021-08-11 16:21:12 +0300

    Update threadPool.h

--- threadPool.h
+++ threadPool.h
@@ -1,28 +1,31 @@
 #ifndef __THREAD_POOL__
 #define __THREAD_POOL__
 
-#include <pthread.h>
 #include "osqueue.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct thread_pool
 {
-  pthread_mutex_t lock;
-  pthread_cond_t notify; 
-  pthread_t *threads; //array with threads
-  OSQueue *queue;
-  int numOfThreads;
-  int numOfTasks; 
-  int head;
-  int tail;
-  int shutdown; //says if the pool is shutting down
+ OSQueue *tasksQueue;
+ pthread_t* threads;
+ pthread_mutex_t mutex;
+ pthread_cond_t cond;
+ int threadsCounter;
+ int shouldStop;
+ int destroyd;
 }ThreadPool;
+
+typedef struct {
+	void (*compFunc)(void *);
+	void *params;
+} Task;
 
 ThreadPool* tpCreate(int numOfThreads);
 
 void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks);
 
 int tpInsertTask(ThreadPool* threadPool, void (*computeFunc) (void *), void* param);
-
-int threadpool_free(ThreadPool* pool);
 
 #endif
